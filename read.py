bb480f6fe83de5e82515f9e07c9f4b8a2f5dec69
Lynn <yalinhuang9@gmail.com>
2020-06-21 13:34:34 +0800

    calculate comment

--- read.py
+++ read.py
@@ -6,8 +6,15 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #count = count + 1
 		if count % 10000 == 0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------------------')
-print(data[1])
+comment = []
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		comment.append(line)
+sum_len = 0
+for c in comment:
+	sum_len = sum_len + len(c)
+
+print('每筆留言的平均長度為', sum_len / len(data))
+	
